4c0e1bc1e69356df930f505fee23307d76dddce0
FroSty361 <192673158+FroSty361@users.noreply.github.com>
2026-09-06 22:30:37 -0700

    feat: add debug logs for Render

--- src/keep_alive.py
+++ src/keep_alive.py
@@ -9,11 +9,16 @@ def home():
     return "Running"
 
 def run():
-    import src.main
+    try:
+        import src.main
+    except Exception:
+        import traceback
+        traceback.print_exc()
 
 t = Thread(target=run, daemon=True)
 t.start()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
+
     app.run(host="0.0.0.0", port=port)
--- src/main.py
+++ src/main.py
@@ -5,4 +5,7 @@ from .bot.bot import bot
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+if not TOKEN:
+    raise RuntimeError("No DISCORD_TOKEN Set")
+
 bot.run(TOKEN)
