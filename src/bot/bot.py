aacc7bb1c6093adc0e1bb6699a513370f85d2bf1
FroSty361 <192673158+FroSty361@users.noreply.github.com>
2026-09-06 21:34:34 -0700

    fix: move keep_alive call. add gunicorn to requirements.txt

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -6,7 +6,6 @@ from dataManagement.runtime_data_management import register_user
 from botProcesses.bot_process_definitions import Processes
 from botProcesses.kana_practice_process import KanaPracticeProcess
 from botProcesses.icon_vocab_process import IconVocabProcess
-from keep_alive import keep_alive
 
 intents = discord.Intents.all()
 intents.message_content = True
@@ -15,8 +14,6 @@ intents.messages = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 TEST_GUILD = discord.Object(id=1515433987105886298)
-
-keep_alive()
 
 @bot.event
 async def on_ready():
--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import os
 from dotenv import load_dotenv
 from bot.bot import bot
+from keep_alive import keep_alive
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 if __name__ == "__main__":
+    keep_alive()
+
     bot.run(TOKEN)
